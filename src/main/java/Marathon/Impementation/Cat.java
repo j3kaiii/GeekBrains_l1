e1340436139f087d76c8fd3ec094bccf4c3089ad
Evgen <j3kaiii@gmail.com>
2019-05-02 20:41:54 +0300

    Delete Cat.java

--- src/main/java/Marathon/Impementation/Cat.java
+++
@@ -1,9 +0,0 @@
-package Marathon.Impementation;
-
-import Marathon.Interface.Animal;
-
-public class Cat extends Animal {
-    public Cat(String name) {
-        super("Кот", name, 200, 20, 0);
-    }
-}
